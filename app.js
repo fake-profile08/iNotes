1df9f952602eff5760c9c1c7ed8e0ba06b2679bf
Yuvraj Singh <anshyuve@gmail.com>
2021-11-26 15:14:46 +0530

    Added a basic template to app.js

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/iNotes')
+const port = 80;
+const pug = require('pug');
+const path = require('path');
+mongoose.connect('mongodb://localhost:27017/iNotes');
+
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to database");
+});
+
+app.get('/',(req,res) => {
+    const params = {
+        title:"Home"
+    };
+    res.status = 200;
+    res.render('index',params)
+});
+
+app.listen(port,() => {
+    console.log("Server is online");
+})
